6feff069a87e278f7bd62399c7162fd7f9b44d53
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-02-09 08:40:45 +0530

    Update CO1pg3c.py

--- CO1pg3c.py
+++ CO1pg3c.py
@@ -1,10 +1,8 @@
  #List comprehensions:
     #(c)Form a list of vowels selected from a given word
 
-word =str(input("Enter the word :"))
-   
-print("The original string is : "+word) 
-  
-res = [idx for idx, ele in enumerate(word) if ele in "aeiou"] 
-  
-print("The vowel indices are : " + str(res))  
+w=input("Enter a word")
+v=["a","e","i","o","u"];
+for i in range(1,len(w)):
+    if w[i]=="a" or w[i]=="e" or w[i]=="i" or w[i]=="o" or w[i]=="u" or w[i]=="A" or w[i]=="E" or w[i]=="I" or w[i]=="O" or w[i]=="U":
+        print(i,w[i] )
