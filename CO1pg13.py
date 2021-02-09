130ffb271e9f22724c9c4f7842d9e8ba61d432eb
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-02-09 08:23:26 +0530

    Update CO1pg13.py

--- CO1pg13.py
+++ CO1pg13.py
@@ -1,4 +1,11 @@
 #Create a list of colors from comma-separated color names entered by user.Display  first and last colors.  
 
-color_list = ["Red","Green","White" ,"Black","pink","Blue"]
-print( "%s %s"%(color_list[0],color_list[-1]))
+n=int(input("Enter a limit:"))
+print(f"Enter {n} colour names:")
+c=[]
+for i in range(0,n):
+    c.append(input())
+for i in range(0, n):
+    print(c[0],c[n-1])
+    break
+
