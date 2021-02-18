641899758e8f00152d452321cc46ece74e18547f
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-02-18 08:08:54 +0530

    Update CO2 PG5.py

--- CO2 PG5.py
+++ CO2 PG5.py
@@ -1,9 +1,10 @@
 n=int(input("Enter a limit:"))
 for i in range(1,n+1):
     for j in range(0,i+1):
-        a=i*j
-        if a==0:
+        x=i*j
+        if x==0:
             continue
         else:
-            print(a,end=" ")
+            print(x,end=" ")
     print("\n")
+
