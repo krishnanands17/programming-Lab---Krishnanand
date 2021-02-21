509b3001bbdf3ba879d37735db610b51d12b5219
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-02-21 20:26:49 +0530

    Update CO1pg3b.py

--- CO1pg3b.py
+++ CO1pg3b.py
@@ -1,17 +1,7 @@
-#List comprehensions:
-  #(b) Square of N number
-
-n=int(input("Enter a limit:"))
-lst=[]
-print(f"Enter {n} numbers")
-for i in range(1,n+1):
-    a=int(input())
-    lst.append(a)
-for i in range(0,n):
-     print(lst[i]*lst[i])
-
-
-
-
-
- 
+x=int(input("Enter the limit:"))
+print(f"Enter {x} Elements:")
+a=[]
+for i in range(0,x):
+    a.append(int(input()))
+for i in range(0,x):
+    print(a[i]*a[i])
