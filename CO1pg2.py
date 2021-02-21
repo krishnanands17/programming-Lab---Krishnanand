48c920b92f89264ab2e6142eb776b455a3cab3cc
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-02-21 18:03:32 +0530

    Update CO1pg2.py

--- CO1pg2.py
+++ CO1pg2.py
@@ -1,20 +1,9 @@
-#Display future leap years from current year to a final year entered by user. 
-
-s=2021
-e = int(input("Enter end year: "))
-
-if(s<e):
+a=2021
+b=int(input("Enter the End Year:"))
+if(a<b):
     print("Leap year are:")
-    for i in range(s, e):
-        if (i % 4 == 0 and i % 100 != 0):
+    for i in range(a,b):
+        if(i % 4 == 0 and i % 100 != 0):
             print(i)
 else:
     print("Invalid Year")
-
-
-
-
-
-
-
-
