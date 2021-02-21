6f775f7444808ef4bd9ae5b65f457578b4563686
krishnanands17 <76091927+krishnanands17@users.noreply.github.com>
2021-02-21 20:20:06 +0530

    Update CO1pg3a.py

--- CO1pg3a.py
+++ CO1pg3a.py
@@ -1,15 +1,10 @@
-#List comprehensions:
-# (a) Generate positive list of numbers from a given list of integers
-
-c=int(input("Enter the limit"))
-print("Enter some integers:")
-lst=[]
-for i in range(0,c):
-    lst.append(int(input()))
-for i in lst:
-    if i<0:
+x=int(input("Enter the limit:"))
+print("Enter the Elements")
+a=[]
+for i in range(0,x):
+    a.append(int(input()))
+for i in a:
+     if i<0:
         continue;
-    else:
+     else:
         print(i)
-
-
